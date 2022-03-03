cf18fb3ad59c9b785ea64460f28c35e5014cb151
Kamila Banaczek-Rybicka <kam.rybicka@gmail.com>
2022-03-03 11:00:05 +0100

    wykonanie zadania 29-js-suma-zmiennych

---
+++ 29-js-suma-zmiennych/main.js
@@ -0,0 +1,10 @@
+let firstNumber = 10;
+let secondNumber = 48;
+
+function numbers(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+
+console.log(numbers(firstNumber, secondNumber));
+// console.log(numbers(10, 59));
